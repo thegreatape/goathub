aa2f056ce591796042ebf93c714ba606287e45ab
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2011-04-09 14:42:36 -0400

    don't re-create already stored news items

--- app/models/news_feed.rb
+++ app/models/news_feed.rb
@@ -14,47 +14,51 @@ class NewsFeed < ActiveRecord::Base
 
   def create_news_items(feed)
     doc = REXML::Document.new(feed)
+
     entries = REXML::XPath.match(doc.root, '//feed/entry') 
     entries.each do |entry|
-      item = NewsItem.new
-      entry.each_recursive do |node|
-        case node.name
-        when 'link'
-          item.link = node.attribute('href').value
-        when 'author'
-          node.each_element do |elem|
-            item.author_name = elem.text if elem.name == 'name'
-            item.author_link = elem.text if elem.name == 'uri'
+      date = DateTime.parse(entry.elements['updated'].text) 
+      if !self.last_updated || date > self.last_updated
+        item = NewsItem.new(:date => date)
+        entry.each_recursive do |node|
+          case node.name
+          when 'link'
+            item.link = node.attribute('href').value
+          when 'author'
+            node.each_element do |elem|
+              item.author_name = elem.text if elem.name == 'name'
+              item.author_link = elem.text if elem.name == 'uri'
+            end
+          when 'title'
+            item.title = node.text
+            match = node.text.match(/on (\w+\/\w+)/)
+            if match
+              item.project_name = match[1]
+              item.project_link = "https://github.com/#{match[1]}"
+            end
+          when 'content'
+            # <content> contains entity-escaped HTML  
+            content_doc = REXML::Document.new(node.text)
+            REXML::XPath.match(content_doc.root, "//div[@class='message']").each do |message|
+              item.message = message.children.to_s
+            end
+          when 'thumbnail'
+            item.thumb_url = node.attribute('url').value
           end
-        when 'title'
-          item.title = node.text
-          match = node.text.match(/on (\w+\/\w+)/)
-          if match
-            item.project_name = match[1]
-            item.project_link = "https://github.com/#{match[1]}"
-          end
-        when 'content'
-          # <content> contains entity-escaped HTML  
-          content_doc = REXML::Document.new(node.text)
-          REXML::XPath.match(content_doc.root, "//div[@class='message']").each do |message|
-            item.message = message.children.to_s
-          end
-        when 'thumbnail'
-          item.thumb_url = node.attribute('url').value
-        when 'updated'
-          item.date = DateTime.parse(node.text)
         end
-      end
-      item.news_feed_id = self.id
-      item.save
-      #if !item.save
+        item.news_feed_id = self.id
+        item.save
+        #if !item.save
         #puts "--- didn't save! ---"
         #puts item.errors.full_messages
-      #else 
+        #else 
         #puts "saved #{item.title}"
-      #end
-
+        #end
+      end
     end
+
+    self.last_updated = DateTime.parse(doc.root.elements['updated'].text)
+    self.save
   end
 
 end
--- test/unit/news_feed_test.rb
+++ test/unit/news_feed_test.rb
@@ -24,6 +24,19 @@ class NewsFeedTest < ActiveSupport::TestCase
     assert_match /\s*<blockquote>\s*Segfault when using XML Builder\s*<\/blockquote>\s*/, item.message
   end
 
+  test "parsing news feed updates last_updated" do
+    old_last_update = news_feeds(:yoda_feed)
+    no_last_update = news_feeds(:empty_feed)
+    xml = IO.read('test/fixtures/feeds/single_item.xml')
+    updated = DateTime::parse('2011-04-07T17:14:58-07:00')
+
+    no_last_update.create_news_items(xml)
+    assert_equal updated, no_last_update.last_updated
+
+    old_last_update.create_news_items(xml)
+    assert_equal updated, old_last_update.last_updated
+  end 
+
   test "creating news items from feed with multiple items" do
     feed = news_feeds(:empty_feed)
     xml = IO.read('test/fixtures/feeds/three_items.xml')
@@ -33,10 +46,28 @@ class NewsFeedTest < ActiveSupport::TestCase
     assert_equal 3, feed.news_items(true).length
   end
 
-  #
-  # le stub tests
-  #
-  test "don't re-create existing news items" do
+  test "only create new news items with updated feeds" do
+    first_update = IO.read('test/fixtures/feeds/first_update.xml')
+    second_update = IO.read('test/fixtures/feeds/second_update.xml')
+    feed = news_feeds(:empty_feed)
+
+    feed.create_news_items(first_update)
+    assert_equal 35, feed.news_items.count
+
+    # one new item in update
+    feed.create_news_items(second_update)
+    assert_equal 36, feed.news_items(true).count
+  end
+
+  test "don't re-create existing news items with un-updated feeds" do
+    feed = news_feeds(:empty_feed)
+    xml = IO.read('test/fixtures/feeds/three_items.xml')
+
+    feed.create_news_items(xml)
+    assert_equal 3, feed.news_items(true).length
+    assert_no_difference('feed.news_items(true).length') do
+      feed.create_news_items(xml)
+    end
   end
 
   test "retrieving unread news items" do
